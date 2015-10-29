d7bc9a1e530e2c134caf4f6e38ba15e9f03f3d3a
Kunal Vyas <kv.kunalvyas@gmail.com>
2015-10-28 20:31:36 -0400

    tested

--- algorithm/collabfilteringALS.py
+++ algorithm/collabfilteringALS.py
@@ -2,11 +2,12 @@ __author__ = 'kunal'
 
 import sys
 import os
+import redis
 
-os.environ['SPARK_HOME'] = "spark-1.5.1"
-sys.path.append("spark-1.5.1/python/")
-sys.path.append("spark-1.5.1/python/lib/py4j-0.8.2.1-src.zip")
-sys.path.append("spark-1.5.1/python/lib/py4j/")
+os.environ['SPARK_HOME'] = "../spark-1.5.1"
+sys.path.append("../spark-1.5.1/python/")
+sys.path.append("../spark-1.5.1/python/lib/py4j-0.8.2.1-src.zip")
+sys.path.append("../spark-1.5.1/python/lib/py4j/")
 
 try:
     from pyspark import SparkContext, SparkConf
@@ -17,17 +18,40 @@ except ImportError as e:
     print ("Couldn't import Spark Modules", e)
     sys.exit(1)
 
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+try:
+    response = r.client_list()
+except redis.ConnectionError as e:
+    print ("Couldn't connect to Redis", e)
+    sys.exit(1)
+
+
 # SETTING CONFIGURATION PARAMETERS
 config = (SparkConf()
         .setMaster("local")
-        .setAppName("Music Recommender")
-        .set("spark.executor.memory", "16G")
-        .set("spark.driver.memory", "16G")
-        .set("spark.executor.cores", "8"))
+        .setAppName("Artist Recommender")
+        .set("spark.executor.memory", "1G")
+        .set("spark.driver.memory", "1G"))
 sc = SparkContext(conf=config)
 
 # Load and parse the data
-data = sc.textFile("data/ydata-ymusic-user-artist-ratings-v1_0.txt")
+data = sc.textFile("../data/10MBdata")
+
+print r.keys()
+
+def load(text_file):
+    data = sc.load(text_file)
+
+def getVariable(variable_name):
+    server = redis.Redis(connection_pool=r)
+    response = server.get(variable_name)
+    return response
+
+def setVariable(variable_name, variable_value):
+    server = redis.Redis(connection_pool=r)
+    server.set(variable_name, variable_value)
+
+#data = sc.textFile("data/ydata-ymusic-user-artist-ratings-v1_0.txt")
 ratings = data.map(lambda l: l.split('\t')).map(lambda l: Rating(int(l[0]), int(l[1]), float(l[2])))
 
 #Count them
@@ -50,5 +74,5 @@ MSE = ratesAndPreds.map(lambda r: (r[1][0] - r[1][1])**2).mean()
 print("Mean Squared Error = " + str(MSE))
 
 # Save and load model
-model.save(sc, "SavedModel")
-#sameModel = MatrixFactorizationModel.load(sc, "/Users/kunal/Developer/MusicRecommender")
+model.save(sc, "Trained Model")
+
---
+++ algorithm/test.py
@@ -0,0 +1,37 @@
+__author__ = 'kunal'
+
+import sys
+import os
+import redis
+from numpy import array
+
+os.environ['SPARK_HOME'] = "../spark-1.5.1"
+sys.path.append("../spark-1.5.1/python/")
+sys.path.append("../spark-1.5.1/python/lib/py4j-0.8.2.1-src.zip")
+sys.path.append("../spark-1.5.1/python/lib/py4j/")
+
+try:
+    from pyspark import SparkContext, SparkConf
+    from pyspark.mllib.recommendation import ALS, MatrixFactorizationModel, Rating
+    print ("Apache-Spark v1.5.1 said, \"All modules found and imported successfully.")
+
+except ImportError as e:
+    print ("Couldn't import Spark Modules", e)
+    sys.exit(1)
+
+# SETTING CONFIGURATION PARAMETERS
+config = SparkConf()
+sc = SparkContext(conf=config)
+
+test = sc.textFile("../data/testdata10MB")
+model = MatrixFactorizationModel.load(sc, "../SavedModel")
+ratings = test.map(lambda line: array([float(x) for x in line.split('\t')]))
+testdata = ratings.map(lambda p: (int(p[0]), int(p[1])))
+predictionsRDD = model.predictAll(testdata).map(lambda r: ((r[0], r[1]), r[2]))
+
+predictions = predictionsRDD.collect()
+print predictions
+
+ratesAndPreds = ratings.map(lambda r: ((r[0], r[1]), r[2])).join(predictionsRDD)
+MSE = ratesAndPreds.map(lambda r: (r[1][0] - r[1][1])**2).reduce(lambda x, y: x + y)/ratesAndPreds.count()
+print("Mean Squared Error = " + str(MSE))
